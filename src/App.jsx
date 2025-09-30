539af711976173782392a16add10521ec93798d3
dhulo_11 <dhruvanpatel8866@gmail.com>
2025-09-30 16:33:02 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SmokeyCursor from "./components/lightswind/smokey-cursor"
 import ModelViewer from './components/lightswind/3d-model-viewer';
-import model from '../public/models/car.glb';
+import model from '../public/models/eunomia.glb';
 
 export default function LoginPage() {
   return (
